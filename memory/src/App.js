bace2f9cf060e243f4aed7c073c51a4d107f8bd0
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2018-04-16 20:13:06 -0500

    images clicking

--- memory/src/App.js
+++ memory/src/App.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
-import Design from './components/design/design.js';
+import Design from './components/design';
 import logo from './logo.svg';
 import design from './design.json';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    design,
+    clickCounts: 0
+  };
+
+  clickImage = id => {
+    const design = this.state.design.filter(design => design.id !==id);
+    this.setState({ clickCounts: 1});
+    console.log(this.state.clickCounts);
+  }
+
+
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Memory Game</h1>
         </header>
-        <Design />
+        {this.state.design.map(design => (
+          <Design image = {design.image}
+          id = {design.id}
+          clickImage = {this.clickImage}/>
+          
+
+        ))}
+
         <p className="App-intro"></p>
       </div>
     );
--- memory/src/components/design/design.js
+++ memory/src/components/design/design.js
@@ -1,15 +1,21 @@
 import React, {Component} from "react";
 import './design.css';
 
+const Design = props => (
+    <div>
+        <img className="design" src = {props.image} onClick={()=>props.clickImage(props.id)}/>
+    </div>
+)
 
 
 
 
-class Design extends Component {
-    render() {
-        return (
-            <div className = "design">Hello</div>
-        )
-    }
-}
+
+// class Design extends Component {
+//     render() {
+//         return (
+//             <div className = "design">Hello</div>
+//         )
+//     }
+// }
 export default Design;
